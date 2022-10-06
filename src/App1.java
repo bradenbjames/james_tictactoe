ce413cf669ea96c98048ad16b8722e549dfc600e
Braden James <bradenjames123@gmail.com>
2022-10-05 21:06:05 -0500

    game, controller setup

--- src/App1.java
+++ src/App1.java
@@ -1,4 +1,6 @@
+import cs331Helper.cs331TicTacToeBoard;
 import cs331Helper.cs331TicTacToeFrame;
+import cs331Helper.cs331TicTacToeGame;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -7,8 +9,13 @@ public class App1 extends Application {
     public void start(Stage stage) {
 
         cs331TicTacToeFrame frame = new cs331TicTacToeFrame(stage);
-
         // Add the game here to the frame
+        cs331TicTacToeGame game1 = new myTicTacToe() {
+        };
+        frame.addGame(game1);
+
+        Players p1 = new Players("X");
+        Players p2 = new Players("O");
 
     }
 
---
+++ src/Players.java
@@ -0,0 +1,23 @@
+import javax.xml.namespace.QName;
+
+import cs331Helper.cs331TicTacToeBoard;
+import cs331Helper.cs331TicTacToeController;
+import cs331Helper.cs331TicTacToePlayer;
+
+public class Players implements cs331TicTacToePlayer {
+    public int maxTries;
+    String symbol = "";
+    cs331TicTacToeBoard sharedBoard = new cs331TicTacToeBoard();
+    cs331TicTacToeController sharedController = new cs331TicTacToeController();
+
+    Players(String s) {
+        this.symbol = s;
+    }
+
+    @Override
+    public void selectSquare(int arg0, int arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
